bdb9c4f8e57821ed60b0f36b1c9730e1b3abe373
Asif-91 <asifchowdhury323@gmail.com>
2020-12-23 21:10:00 +0600

    adding inheritance

--- JSBasics/ES6/ES6Practice.js
+++ JSBasics/ES6/ES6Practice.js
@@ -81,3 +81,91 @@
     console.log(result2);
     console.log(result3);
     console.log(result4);
+
+    // spread operator
+    //===================
+
+    const ages = [25, 28, 30, 31, 29];
+    const ages2 = [ 34, 32];
+    const ages3 = [ 40, 42];
+    const allAges = ages.concat(ages2).concat(ages3);
+    const allAges2 = [...ages, ...ages2, 5, ...ages3];
+    // console.log(allAges2);
+
+    const business = 650;
+    const minister = 450;
+    const shochib = 250;
+    const takaPoisa = [650, 450, 250, 890, 999];
+    // const maximum = Math.max(business, minister, shochib);
+    const maximum = Math.max(...takaPoisa);
+    console.log(maximum);
+
+    // Class, constructor, create object from class
+    //====================================================
+
+    class Student{
+        constructor (sId, sName){
+            this.id = sId;
+            this.name = sName;
+            this.school = "Kolimunnesa School"
+        }
+    }
+    const student1 = new Student (12, "Shuvo");
+    const student2 = new Student (13, "Sifali");
+    const student3 = new Student (14, "Bappi");
+    console.log(student2);
+
+    // Inheritance, extends class, super, class method
+    //=========================================================
+    
+    class Parent{
+        constructor(){
+            this.fatherName = "Schwerznegger"
+        }
+        getFullName(){
+            return this.name +" "+this.fatherName;
+        }
+    }
+
+    class Child extends Parent{
+        constructor(name){
+            super();
+            this.name = name;       
+        }
+    }
+
+    const baby = new Child("Arnold");
+    const baby2 = new Child("Tom");
+    console.log(baby.getFullName());
+    console.log(baby2.getFullName());
+
+    // destructure, Object, array, destructure complex object
+    //===========================================================
+
+    const person = {name: 'Tanvir', age: 28, Job: 'CGS', gfName: 'Boudi', address: 'Kotuwali', Phone: '01676252882', Friends: ["Asif", "Mash", "Munna", "Taisir"]}
+
+    const {Phone, gfName, Friends} = person;
+    //========================================
+    const complexObject = {
+        name: 'struggle',
+        info: {
+            address: 'anyWhere',
+            leader: 'anyOne'
+        }
+    }
+
+    const {leader} = complexObject.info;
+    console.log(leader);
+
+    // const Phone = person.Phone;
+    // const gfName = person.gfName;
+    // const Friends = person.Friends;
+
+    // console.log(gfName, Phone);
+    // console.log(Friends, Phone);
+
+
+    const friends = ["jim","Asif", "Mash", "Munna", "Taisir"];
+    const [chutoFriend, ...olderFriend] = friends;
+    console.log(...olderFriend);
+    
